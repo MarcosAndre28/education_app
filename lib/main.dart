6d43e5269fbcd9787db060be9f26d1f5f42c631b
MarcosAndre28 <marcosandre1728@gmail.com>
2024-04-14 17:07:38 -0300

    organized import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:education_app/core/res/colours.dart';
 import 'package:education_app/core/res/fonts.dart';
-import 'package:education_app/src/on_boarding/presentation/on_boarding_screen.dart';
+import 'package:education_app/core/services/router.dart';
 import 'package:flutter/material.dart';
-
-import 'core/services/router.dart';
 
 void main() {
   runApp(const MyApp());
